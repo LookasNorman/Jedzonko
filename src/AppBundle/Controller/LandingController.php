6223985914de6434ffb2ecfd03d4009e9c0c69d9
konrad szukalski <core.editor>
2019-07-31 08:49:12 +0200

    dashboard link updated in controller

--- src/AppBundle/Controller/LandingController.php
+++ src/AppBundle/Controller/LandingController.php
@@ -4,9 +4,15 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class LandingController extends Controller
 {
+    /**
+     * @return Response
+     * @Route("/")
+     */
     public function indexAction(): Response
     {
         return $this->render('landing/index.html.twig', []);

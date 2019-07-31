2632d37a2fcb01315ff1cd6bfa768849f17be2d8
konrad szukalski <core.editor>
2019-07-31 13:16:46 +0200

    * routes in RecipeController added

--- src/AppBundle/Controller/RecipeController.php
+++ src/AppBundle/Controller/RecipeController.php
@@ -4,24 +4,44 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class RecipeController extends Controller
 {
+    /**
+     * @return Response
+     * @Route("/recipe/add/")
+     *
+     */
     public function addAction(): Response
     {
         return $this->render('dashboard/recipe/add.html.twig', []);
     }
-
+    
+    /**
+     * @return Response
+     * @Route("recipe/modify/{id}")
+     */
     public function editAction(): Response
     {
         return $this->render('dashboard/recipe/edit.html.twig', []);
     }
-
+    
+    
+    /**
+     * @return Response
+     * @Route ("/recipe/list/{id}")
+     */
     public function detailsAction(): Response
     {
         return $this->render('dashboard/recipe/details.html.twig', []);
     }
-
+   
+    /**
+     * @return Response
+     * @Route ("/recipe/list")
+     */
     public function listAction(): Response
     {
         return $this->render('dashboard/recipe/list.html.twig', []);

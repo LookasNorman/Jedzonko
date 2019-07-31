e91778d5031f846d0b13b29bed376993dbb18049
adamssef <adam33ef@gmail.com>
2019-08-01 00:26:23 +0200

    landing twig added to landing homepage route /

--- src/AppBundle/Controller/LandingController.php
+++ src/AppBundle/Controller/LandingController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Recipe;
+
 
 
 class LandingController extends Controller
@@ -15,7 +17,30 @@ class LandingController extends Controller
      */
     public function indexAction(): Response
     {
-        return $this->render("landing/index.html.twig");
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('AppBundle:Recipe');
+        $allRecipes = $repository->findAll();
+
+        for ($i = 1; $i <= count($allRecipes); $i++) {
+            $allRecipesLengthArray[] = $i;
+        }
+        shuffle($allRecipesLengthArray);
+
+        $firstRandomRecipeName = ($allRecipes[$allRecipesLengthArray[0]])->getName();
+        $firstRandomRecipeDesc = ($allRecipes[$allRecipesLengthArray[0]])->getDescription();
+        $secondRandomRecipeName = ($allRecipes[$allRecipesLengthArray[1]])->getName();
+        $secondRandomRecipeDesc = ($allRecipes[$allRecipesLengthArray[1]])->getDescription();
+        $thirdRandomRecipeName = ($allRecipes[$allRecipesLengthArray[2]])->getName();
+        $thirdRandomRecipeDesc = ($allRecipes[$allRecipesLengthArray[2]])->getDescription();
+
+        return $this->render("landing/index.html.twig", [
+            "firstRecipeName" => $firstRandomRecipeName,
+            "firstRecipeDesc" => $firstRandomRecipeDesc,
+            "secondRecipeName" => $secondRandomRecipeName,
+            "secondRecipeDesc" => $secondRandomRecipeDesc,
+            "thirdRecipeName" => $thirdRandomRecipeName,
+            "thirdRecipeDesc" => $thirdRandomRecipeDesc
+        ]);
     }
     
     /**

9e60f51168a8dc0a415f9293ca4bd7aea556c0ce
konrad szukalski <core.editor>
2019-08-03 13:20:13 +0200

    last Plan in controller done

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bridge\Doctrine\Form\ChoiceList\ORMQueryBuilderLoader;
 use AppBundle\Entity\Plan;
 use AppBundle\Entity\Recipe;
+use AppBundle\Entity\RecipePlan;
 
 /**
  * @Route("/main")
@@ -33,31 +34,89 @@ class DashboardController extends Controller
         //last Plan Name
         $query = $em->createQuery('SELECT p FROM AppBundle\Entity\Plan p ORDER BY p.created DESC');
         $plans = $query->getResult();
-        $lastPlanId=$plans[0]->getId();
-        $lastPlanName=$plans[0]->getName();
+        $lastPlanId = $plans[0]->getId();
+        $lastPlanName = $plans[0]->getName();
         
         
         return $this->render('dashboard/index.html.twig', [
             'noPlans' => $noPlans,
-            'noRecipes'=>$noRecipes,
-            'lastPlanName'=>$lastPlanName]);
+            'noRecipes' => $noRecipes,
+            'lastPlanName' => $lastPlanName]);
     }
     
     /**
      * @Route("/lastPlan")
      */
-    public function lastPlanAction():Response
+    public function lastPlanAction(): Response
     {
-        $em=$this->getDoctrine()->getManager();
-        //$query = $em->createQuery('SELECT u FROM MyProject\Model\User u WHERE u.age > 20');
+        $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery('SELECT p FROM AppBundle\Entity\Plan p ORDER BY p.created DESC');
         $plans = $query->getResult();
-        $lastPlanId=$plans[0]->getId();
-        $lastPlanName=$plans[0]->getName();
+        $lastPlanId = $plans[0]->getId();
+        $lastPlanName = $plans[0]->getName();
         
-        var_dump($recentPlanName);
+        $recipeplan = $plans[0]->getRecipePlans();
         
-        return new Response();
-    }
+        $query = $em->createQuery
+        ('SELECT rp FROM AppBundle\Entity\RecipePlan rp  JOIN rp.plan p WHERE p.id=4');
+        //('SELECT rp FROM AppBundle\Entity\RecipePlan rp  JOIN rp.plan p WHERE p.id=4');
+        $rp = $query->getResult();
+        
+        $query2 = $em->createQuery
+        ('SELECT  (rp.dayName), count(rp.dayName) FROM AppBundle\Entity\RecipePlan rp  JOIN rp.plan p WHERE p.id=4 group by rp.dayName');
+        
+        
+        $res = $query2->getResult();
+        var_dump($res);
+        //var_dump($res[0]);
+        //id day name        // count of id day name
+        // echo $res[0][1].'->'.$res[1][1];echo'<br>';
+        // echo $res[0][2].'->'.$res[1][2];
+        //var_dump($rp);
+        //var_dump($rp[5]->getDayName()->getId());
+        
+        
+        // echo$rp[$b]->getDayName()->getDayName();echo'<br>';
+        
+        for ($a = 0; $a < count($res); $a++) // petla pocalym result
+        {
+           
+            
+            $id_day = $res[$a][1];
+            $licznik = $res[$a][2];
+            echo 'id_day=' . $id_day . 'licznik='.$licznik.' <br>'; //to jest id_day
     
+            for($x=0;$x<count($rp);$x++) {
+                if ($id_day == $rp[$x]->getDayName()->getId()) {
+                    echo $rp[$x]->getDayName()->getDayName();break;
+                }
+            }
+            //wypisujemy naglowek
+           
+           
+            for ($b = 0; $b < count($rp); $b++)
+            {
+                if ($id_day == $rp[$b]->getDayName()->getId()) {
+                    //echo $rp[$b]->getDayName()->getDayName();
+                    echo '<br>';
+                    echo $rp[$b]->getMealName();
+                    echo ' - ';
+                    echo $rp[$b]->getRecipe()->getName();
+                    echo '<br>';
+                }
+                
+                
+            }
+        }
+        
+        
+        
+        
+        return $this->render('dashboard/test.html.twig', ['lastplan' => $plans[0],
+                'rp' => $rp,
+                'res' =>$res]
+        );
+        
+        
+    }
 }

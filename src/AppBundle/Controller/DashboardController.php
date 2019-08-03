25916717d91b44886978e7a3ccee4914bb2d50d2
konrad szukalski <core.editor>
2019-08-03 14:16:37 +0200

    - last Plan controller cleaned and twig view changed

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -36,87 +36,28 @@ class DashboardController extends Controller
         $plans = $query->getResult();
         $lastPlanId = $plans[0]->getId();
         $lastPlanName = $plans[0]->getName();
-        
+    
+    //last plan details
+        $query = $em->createQuery
+        ('SELECT rp FROM AppBundle\Entity\RecipePlan rp  JOIN rp.plan p WHERE p.id=4 order by rp.mealOrder,rp.dayName');
+        $rp = $query->getResult();
+    
+        $query2 = $em->createQuery
+        ('SELECT  (rp.dayName), count(rp.dayName) FROM AppBundle\Entity\RecipePlan rp  JOIN rp.plan p
+        WHERE p.id=4 group by rp.dayName');
+    
+        $res = $query2->getResult();
         
         return $this->render('dashboard/index.html.twig', [
             'noPlans' => $noPlans,
             'noRecipes' => $noRecipes,
-            'lastPlanName' => $lastPlanName]);
+            'lastPlanName' => $lastPlanName,
+            'rp' => $rp,
+            'res' =>$res
+            ]);
     }
     
-    /**
-     * @Route("/lastPlan")
-     */
-    public function lastPlanAction(): Response
-    {
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery('SELECT p FROM AppBundle\Entity\Plan p ORDER BY p.created DESC');
-        $plans = $query->getResult();
-        $lastPlanId = $plans[0]->getId();
-        $lastPlanName = $plans[0]->getName();
-        
-        $recipeplan = $plans[0]->getRecipePlans();
-        
-        $query = $em->createQuery
-        ('SELECT rp FROM AppBundle\Entity\RecipePlan rp  JOIN rp.plan p WHERE p.id=4');
-        //('SELECT rp FROM AppBundle\Entity\RecipePlan rp  JOIN rp.plan p WHERE p.id=4');
-        $rp = $query->getResult();
-        
-        $query2 = $em->createQuery
-        ('SELECT  (rp.dayName), count(rp.dayName) FROM AppBundle\Entity\RecipePlan rp  JOIN rp.plan p WHERE p.id=4 group by rp.dayName');
-        
-        
-        $res = $query2->getResult();
-        var_dump($res);
-        //var_dump($res[0]);
-        //id day name        // count of id day name
-        // echo $res[0][1].'->'.$res[1][1];echo'<br>';
-        // echo $res[0][2].'->'.$res[1][2];
-        //var_dump($rp);
-        //var_dump($rp[5]->getDayName()->getId());
-        
-        
-        // echo$rp[$b]->getDayName()->getDayName();echo'<br>';
-        
-        for ($a = 0; $a < count($res); $a++) // petla pocalym result
-        {
-           
-            
-            $id_day = $res[$a][1];
-            $licznik = $res[$a][2];
-            echo 'id_day=' . $id_day . 'licznik='.$licznik.' <br>'; //to jest id_day
     
-            for($x=0;$x<count($rp);$x++) {
-                if ($id_day == $rp[$x]->getDayName()->getId()) {
-                    echo $rp[$x]->getDayName()->getDayName();break;
-                }
-            }
-            //wypisujemy naglowek
-           
-           
-            for ($b = 0; $b < count($rp); $b++)
-            {
-                if ($id_day == $rp[$b]->getDayName()->getId()) {
-                    //echo $rp[$b]->getDayName()->getDayName();
-                    echo '<br>';
-                    echo $rp[$b]->getMealName();
-                    echo ' - ';
-                    echo $rp[$b]->getRecipe()->getName();
-                    echo '<br>';
-                }
-                
-                
-            }
-        }
-        
-        
-        
-        
-        return $this->render('dashboard/test.html.twig', ['lastplan' => $plans[0],
-                'rp' => $rp,
-                'res' =>$res]
-        );
-        
         
     }
-}
+

53fd0103892fbbfaaa0aa2f6ac1ec99ded55536d
LookasNorman <lookasziebice@gmail.com>
2019-08-03 16:10:01 +0200

    A vote for the recipe has been added.

--- src/AppBundle/Controller/RecipeController.php
+++ src/AppBundle/Controller/RecipeController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Recipe;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +20,7 @@ class RecipeController extends Controller
     {
         return $this->render('dashboard/recipe/add.html.twig', []);
     }
-    
+
     /**
      * @return Response
      * @Route("recipe/modify/{id}")
@@ -27,17 +29,34 @@ class RecipeController extends Controller
     {
         return $this->render('dashboard/recipe/edit.html.twig', []);
     }
-    
-    
+
+
     /**
      * @return Response
-     * @Route ("/recipe/{id}")
+     * @Route ("/recipe/{id}", methods={"POST"})
      */
-    public function detailsAction(): Response
+    public function detailsAction(Request $request): Response
     {
-        return $this->render('dashboard/recipe/details.html.twig', []);
+        $plus = $request->request->get('plus');
+        $minus = $request->request->get('minus');
+        $recipeId = $request->request->get('recipeId');
+        $em = $this->getDoctrine()->getManager();
+        $recipe = $em->getRepository(Recipe::class)->find($recipeId);
+
+        if ($plus) {
+            $recipe->setVotes($recipe->getVotes() + 1);
+        }
+        if ($minus) {
+            $recipe->setVotes($recipe->getVotes() - 1);
+        }
+        $em->persist($recipe);
+        $em->flush();
+
+        return $this->redirectToRoute('recipe_details', [
+            'id' => $recipeId
+        ]);
     }
-   
+
     /**
      * @return Response
      * @Route ("/recipe/list")

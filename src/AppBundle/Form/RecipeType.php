e1752c8c540926a0918fc61554eb239ed361eed3
LookasNorman <lookasziebice@gmail.com>
2019-08-03 19:47:07 +0200

    Added recipe form.

---
+++ src/AppBundle/Form/RecipeType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Ingredients;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextType::class)
+            ->add('recipePreparationMethod', TextType::class)
+            ->add('preparationTime', NumberType::class)
+            ->add('ingredients', EntityType::class, [
+                'class' => Ingredients::class,
+                'choice_label' => 'ingredient',
+                'placeholder' => 'Choose an ingredient'
+            ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Recipe'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_recipe';
+    }
+}
